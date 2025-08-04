41fae252c4c90960eb234e1bbf629e4ed2947817
subha <snag28898@gmail.com>
2025-08-05 04:16:08 +0530

    by GitStuff

---
+++ src/coding/RestApiTest.java
@@ -0,0 +1,18 @@
+package coding;
+
+public class RestApiTest {
+
+	
+	public void Test1() {
+		System.out.println("Test1");
+	}
+	
+	public void test2() {
+		System.out.println("Test2");
+	}
+	
+	public void test3() {
+		System.out.println("User Deo 3 add a Test Cases");
+	}
+}
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module CodeExercise2 {
+}
