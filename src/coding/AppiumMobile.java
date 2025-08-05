14dede2f8727b3434ba37734a8379eacc9874ee1
subha <snag28898@gmail.com>
2025-08-06 03:29:55 +0530

    Architect develop by GitAutomation user

---
+++ src/coding/AppiumMobile.java
@@ -0,0 +1,15 @@
+package coding;
+
+public class AppiumMobile {
+
+	
+	public void iphone() {
+		
+		System.out.println("Its my Iphone");
+	}
+	
+	public void samsung() {
+		
+		System.out.println("Its my samsung");
+	}
+}
